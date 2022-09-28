4bbb80d20d5a1128cef349333202dd612e56f1ad
wy20191 <wy20191@outlook.com>
2022-09-28 13:46:48 +0800

    完成商品列表模块

---
+++ components/my-goods/my-goods.vue
@@ -0,0 +1,78 @@
+<template>
+	<view>
+		<view class="goods-list-item" @click="click">
+			<view class="left-item">
+				<image :src="goods.goods_small_logo || defaultPic" mode=""></image>
+			</view>
+			<view class="right-item">
+				<view class="title">
+					{{goods.goods_name}}
+				</view>
+				<view class="price">
+					￥{{goods.goods_price | tofixed}}
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "my-goods",
+		props: {
+			goods: {
+				type: Object,
+				default: {}
+			}
+		},
+		filters: {
+			tofixed(num) {
+				return Number(num).toFixed(2)
+			}
+		},
+		data() {
+			return {
+				// 默认商品图片
+				defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png',
+
+			};
+		},
+		methods: {
+			click() {
+				this.$emit('click')
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.goods-list-item {
+		display: flex;
+		padding: 10rpx;
+		border-bottom: 1px solid #eaeaea;
+
+		.left-item {
+			image {
+				width: 230rpx;
+				height: 200rpx;
+			}
+		}
+
+		.right-item {
+			display: flex;
+			flex-direction: column;
+			justify-content: space-between;
+			padding: 0 10rpx;
+
+			.title {
+				font-size: 24rpx;
+				font-weight: bold;
+			}
+
+			.price {
+				color: #c00000;
+
+			}
+		}
+	}
+</style>
--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,22 +1,75 @@
 <template>
 	<view>
-		
+		<view class="goods-list">
+				<my-goods @click="goodsClickHandle(item.goods_id)" v-for="item in goodslist" :key="item.goods_id" :goods="item"></my-goods>
+		</view>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			}
+export default {
+	data() {
+		return {
+			// 商品列表请求数据
+			queryObj: {
+				query: '',
+				cid: '',
+				pagenum: 1,
+				pagesize: 10
+			},
+			// 列表总数
+			total: 0,
+			// 商品列表
+			goodslist: [],
+			// 节流阀
+			isLoading:false
+		};
+	},
+	onLoad(option) {
+		// 获取关键字
+		this.queryObj.query = option.query || '';
+		// 获取分类id
+		this.queryObj.cid = option.cid || '';
+		// 获取商品列表
+		this.getGoodsList();
+	},
+	onPullDownRefresh() {
+		this.queryObj.pagenum = 1
+		this.queryObj.pagesize = 10
+		this.total = 0
+		this.isLoading = false
+		this.goodslist = []
+		this.getGoodsList(() => uni.stopPullDownRefresh())
+	},
+	onReachBottom() {
+		// 判断数据是否加载完成
+		if(this.queryObj.pagenum * this.queryObj.pagesize > this.total) return uni.$showToast('到底啦!')
+		// 判断是否在请求数据
+		if(this.isLoading) return 
+		this.queryObj.pagenum++
+		this.getGoodsList()
+	},
+	methods: {
+		// 获取商品列表
+		async getGoodsList(sp) {
+			const { data: res } = await uni.$http.get('/api/public/v1/goods/search', this.queryObj);
+			this.isLoading = true
+			if (res.meta.status !== 200) return uni.$showToast();
+			this.goodslist = [...this.goodslist,...res.message.goods];
+			this.total = res.message.total;
+			this.isLoading = false
+			sp && sp()
 		},
-		methods: {
-			
+		// 商品点击回调
+		goodsClickHandle(goods_id) {
+			uni.navigateTo({
+				url:'/subpkg/goods_detail/goods_detail?goods_id=' + goods_id
+			})
 		}
 	}
+};
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+	
 </style>
